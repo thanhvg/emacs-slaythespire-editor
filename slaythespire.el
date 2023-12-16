e248d3ff87a6ef36c7dbfb6495b49683f5c352c4
Thanh Vuong <thanhvg@gmail.com>
2023-12-16 01:48:15 -0700

    nameing

--- slaythespire.el
+++ slaythespire.el
@@ -18,38 +18,39 @@
     (insert-file-contents file)
     (buffer-string)))
 
-(defun slaythespire-decode (str)
+(defun slaythespire--decode (str)
   "return a decoded string"
   (slaythespire--xor-str-against-key (base64-decode-string str) slaythespire-key))
 
-(defun slaythespire-encode (json)
+(defun slaythespire--encode (json)
   (base64-encode-string (slaythespire--xor-str-against-key json slaythespire-key)))
 
 (defun slaythespire-open-save-file (file)
-  "Huh"
+  "Open Slay The Spire save file."
   (interactive "fEnter file path: ")
   (let ((buffer (get-buffer-create (format "*%s*" (file-name-nondirectory file)))))
     (with-current-buffer buffer
       (erase-buffer)
-      (insert (slaythespire-decode (slaythespire--read-file-content file)))
+      (insert (slaythespire--decode (slaythespire--read-file-content file)))
       (json-mode)
       (setq-local slaythespire--save-file file)
       (slaythespire-mode))
     (switch-to-buffer buffer)))
 
 (defun slaythespire-save ()
+  "Save change to file."
   (interactive)
   (let ((str (buffer-string)))
     (with-temp-file slaythespire--save-file
-      (insert (slaythespire-encode str)))))
+      (insert (slaythespire--encode str)))))
 
 (defun slaythespire-reload ()
+  "Reload change from save file."
   (interactive)
   (slaythespire-open-save-file slaythespire--save-file))
 
 (define-minor-mode slaythespire-mode
-  "Toggle Hungry mode.
-...rest of documentation as before..."
+  "Minor mode for Slay The Spire editor."
   ;; The initial value.
   :init-value nil
   ;; The minor mode bindings.
